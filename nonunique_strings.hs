a85fa85c2498ba1f56a7031bc9a4df3cea8a9f7c
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2021-04-24 15:14:48 +0300

    Nonunique strings: updated count of required copies

--- nonunique_strings.hs
+++ nonunique_strings.hs
@@ -1,11 +1,13 @@
-inList:: (Eq a) => a -> [a] -> Bool
-inList el [] = False
-inList el (x:xs) = (el == x) || inList el xs
+countInList:: (Eq a) => a -> [a] -> Int
+countInList el [] = 0
+countInList el (x:xs) 
+    | (el == x) = 1 + countInList el xs
+    | otherwise = countInList el xs
 
 nonuniqueStrings:: [String] -> [String]
 nonuniqueStrings [] = []
 nonuniqueStrings (s:l)
-    | s `inList` l = s : nonuniqueStrings [x | x<-l, x/=s]
+    | (s `countInList` l) > 1 = s : nonuniqueStrings [x | x<-l, x/=s]
     | otherwise    = nonuniqueStrings l
 
 main = interact (unlines. nonuniqueStrings . lines)
